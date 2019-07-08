09d13d4be580396a0c4d32c8f5beb96b17768691
kuhufu <kuhufu@gmail.com>
2019-07-09 00:12:56 +0800

    以秒为单位的周期性更新缓存

---
+++ updater.go
@@ -0,0 +1,46 @@
+package gcache
+
+import (
+	"github.com/kuhufu/scheduler"
+	"time"
+)
+
+type Updater struct {
+	scheduler *scheduler.Scheduler
+	store     CacheStore
+}
+
+func NewUpdaterOf(store CacheStore) *Updater {
+	return &Updater{
+		scheduler: scheduler.New(),
+		store:     store,
+	}
+}
+
+func (u *Updater) Start() {
+	u.scheduler.Start()
+}
+
+func (u *Updater) Stop() {
+	u.scheduler.Stop()
+}
+
+//@param immediately 是否立刻执行一次fetch函数
+func (u *Updater) Interval(key string, seconds int, fetch func() []byte, immediately bool) {
+	if immediately {
+		u.store.Set(key, fetch(), -1)
+	}
+	u.scheduler.AddIntervalFunc(time.Duration(seconds)*time.Second, func() {
+		u.store.Set(key, fetch(), -1)
+	})
+}
+
+//@param immediately 是否立刻执行一次fetch函数
+func (u *Updater) Timeout(key string, seconds int, fetch func() []byte, immediately bool) {
+	if immediately {
+		u.store.Set(key, fetch(), -1)
+	}
+	u.scheduler.AddTimeoutFunc(time.Duration(seconds)*time.Second, func() {
+		u.store.Set(key, fetch(), -1)
+	})
+}
